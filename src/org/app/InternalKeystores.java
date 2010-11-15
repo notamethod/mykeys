a93e9789de6a5b1358605fcf7a1a41b80c6bbcf9
Christophe Roger <croger42@mycompany.com>
2010-11-15 23:38:15 +0000

    adding default AC
    git-svn-id: http://jmykeys.googlecode.com/svn/trunk/jmykeys@22 9e4d3628-fea2-9e54-e3a5-b682eb42eb79

--- src/org/app/InternalKeystores.java
+++ src/org/app/InternalKeystores.java
@@ -7,8 +7,11 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.channels.FileChannel;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.app.KeyStoreInfo.StoreFormat;
 import org.app.KeyStoreInfo.StoreModel;
 import org.app.KeyStoreInfo.StoreType;
@@ -16,6 +19,7 @@ import org.ihm.MyKeys;
 
 
 public class InternalKeystores {
+    public static final Log log = LogFactory.getLog(InternalKeystores.class);   
     public static String password="mKeys983178";
     private static String pathAC;
     private static String pathCert;
@@ -49,29 +53,18 @@ public class InternalKeystores {
 	KeyStoreInfo kinfo = null;
 	File f= new File(path);
 	if (!f.exists()){
-	    FileChannel inChannel = null;
-	    FileChannel outChannel = null;
-	        try {
-	     inChannel =  new FileInputStream(InternalKeystores.class.getResource("/org/config/myKeysAc.jks").getFile()).getChannel();
-            //InternalKeystores.class.getResource("/org/config/myKeysAc.jks").getFile()getChannel();
-         outChannel = new
-            FileOutputStream(f).getChannel();
 
-            inChannel.transferTo(0, inChannel.size(),
-                    outChannel);
+	        try {
+	        	
+	     InputStream is = (InternalKeystores.class.getResourceAsStream("/org/config/mykeysAc.jks"));
+	     copyFile(is, f);
+            //InternalKeystores.class.getResource("/org/config/myKeysAc.jks").getFile()getChannel();
+
         } 
-        catch (IOException e) {
-            e.printStackTrace();
+        catch (Exception e) {
+        	log.error(e);
         }
-        finally {
-            try {
-		if (inChannel != null) inChannel.close();
-		if (outChannel != null) outChannel.close();
-	    } catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	    }
-        }
+
 	    
 //	    try {
 //		InputStream is =  InternalKeystores.class.getResourceAsStream("/org/config/myKeysAc.jks");
@@ -109,5 +102,25 @@ public class InternalKeystores {
 	kinfo.setOpen(true);
 	return kinfo;   
     }    
+    
+    public static void copyFile(InputStream is, File out) throws Exception {
+        //InputStream fis  = new FileInputStream(in);
+        FileOutputStream fos = new FileOutputStream(out);
+        try {
+            byte[] buf = new byte[1024];
+            int i = 0;
+            while ((i = is.read(buf)) != -1) {
+                fos.write(buf, 0, i);
+            }
+        } 
+        catch (Exception e) {
+            throw e;
+        }
+        finally {
+            if (is != null) is.close();
+            if (fos != null) fos.close();
+        }
+      }
+    
 
 }
