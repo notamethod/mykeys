fea7519374280325979bb624bc63930cf5e18330
Christophe Roger <croger42@gmail.com>
2013-10-18 15:08:43 +0000

    new component
    git-svn-id: http://jmykeys.googlecode.com/svn/trunk/jmykeys@58 9e4d3628-fea2-9e54-e3a5-b682eb42eb79

---
+++ src/org/dpr/swingutils/JDropText.java
@@ -0,0 +1,138 @@
+package org.dpr.swingutils;
+
+import java.awt.FlowLayout;
+import java.awt.dnd.DropTarget;
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFileChooser;
+import javax.swing.JPanel;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+public class JDropText extends JPanel  {
+    
+    JDropTextField textfield;
+    JButton chooseButton;
+
+	public JDropText(JComponent jc1, JComponent jc2) {
+		setLayout(new FlowLayout(FlowLayout.LEADING));
+		add(jc1);
+		add(jc2);
+	}
+
+	public JDropText(JComponent jc1, JComponent jc2, int position) {
+		setLayout(new FlowLayout(position));
+		add(jc1);
+		add(jc2);
+	}
+	
+	   public JDropText() {
+	       setLayout(new FlowLayout(FlowLayout.LEADING));
+	        textfield  = new JDropTextField("", 20);
+	        textfield.setEditable(false);
+	        chooseButton = new JButton("...");
+	        add(textfield);
+	        add(chooseButton);
+	        initComponent();
+	    }
+	   
+	   private void initComponent(){
+	       
+	       chooseButton.addActionListener(new GenericAction());
+	       chooseButton.setActionCommand("CHOOSE_IN");	 
+	     
+	       //textfield.addFocusListener(l);
+	        //DocumentListener myListener = new TextListener();
+	        
+	        DropTarget dropTarget = new DropTarget(textfield, textfield);	       
+	   }
+	   
+	   
+	   
+	    public class TextListener implements DocumentListener
+	    {
+
+	        public void changedUpdate(DocumentEvent e)
+	        {
+	            updateOutput();
+
+	        }
+
+	        public void insertUpdate(DocumentEvent e)
+	        {
+
+	            updateOutput();
+
+	        }
+
+	        public void removeUpdate(DocumentEvent e)
+	        {
+	            updateOutput();
+
+	        }
+
+	    }	   
+	   
+	   public class GenericAction extends AbstractAction
+	    {
+
+	        public void actionPerformed(ActionEvent event)
+	        {
+	            String command = event.getActionCommand();
+	          
+
+	            if (command.equals("CHOOSE_IN"))
+	            {
+	                JFileChooser jfc = new JFileChooser();
+
+	                if (jfc.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
+	                {
+	                    textfield.setText(jfc.getSelectedFile().getAbsolutePath());
+
+	                }
+
+	            }
+	        }
+	    }
+
+    /**
+     * .
+     * 
+     *<BR><pre>
+     *<b>Algorithme : </b>
+     *DEBUT
+     *    
+     *FIN</pre>
+     *
+     */
+    public void updateOutput()
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    /**
+     * .
+     * 
+     *<BR><pre>
+     *<b>Algorithme : </b>
+     *DEBUT
+     *    
+     *FIN</pre>
+     *
+     * @return
+     */
+    public String getText()
+    {
+        // TODO Auto-generated method stub
+        return textfield.getText();
+    }
+    
+    public void addListener(DocumentListener myListener){
+        textfield.getDocument().addDocumentListener(myListener);
+    }
+
+}
